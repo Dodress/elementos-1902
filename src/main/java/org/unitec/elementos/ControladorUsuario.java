e76f930e71f2864fd93d12f00d071eb4f214fa84
T-107 <T-107@PC185>
2019-02-15 21:44:57 -0600

    clase de 15 de febrero

---
+++ src/main/java/org/unitec/elementos/ControladorUsuario.java
@@ -0,0 +1,17 @@
+package org.unitec.elementos;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author T-107
+ */
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ControladorUsuario {
+    
+}
