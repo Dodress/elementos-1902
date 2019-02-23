4ebe0945e09f2e911d27fdfb403031295bb52e17
T-107 <T-107@PC185>
2019-02-22 21:41:26 -0600

    Clase del 22

--- src/main/java/org/unitec/elementos/ElementosApplication.java
+++ src/main/java/org/unitec/elementos/ElementosApplication.java
@@ -18,7 +18,17 @@ public class ElementosApplication implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         //qui irán todas nuestras pruebas sin necesidad de interface de usuario(UI)
-        repoU.save(new Usuario(1, "Bryan Loza"));
+        
+        //repoU.save(new Usuario(3, "Karen Arellano")); //guardar
+        //for(Usuario u:repoU.findAll()){           //buscar todos
+          // System.out.println(u);
+        //}
+        //System.out.println(repoU.findById(1).get());      //buscar uno por id
+        //repoU.save(new Usuario(4,"Ana Cruz"));        //actualizar
+        //repoU.deleteById(1);                              //borrar por id
+        //for(Usuario u:repoU.findByNombre("Bryan Loza")){  //buscar uno por nomre
+          //  System.out.println(u);
+        //}
     }
 
 }
--- src/main/java/org/unitec/elementos/RepoUsuario.java
+++ src/main/java/org/unitec/elementos/RepoUsuario.java
@@ -1,5 +1,6 @@
 package org.unitec.elementos;
 
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
@@ -7,5 +8,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * @author T-107
  */
 public interface RepoUsuario extends MongoRepository<Usuario, Integer>{
-    
+    List<Usuario> findByNombre(String nombre);
 }
